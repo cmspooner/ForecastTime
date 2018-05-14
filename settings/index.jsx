d6580f3354ec954a3e50e986f17c107c9f9dbaf8
Chris Spooner <cmspooner@gmail.com>
2018-05-14 08:42:22 -0400

    spelling errors and show temp units

--- settings/index.jsx
+++ settings/index.jsx
@@ -5,16 +5,19 @@ function mySettings(props) {
         title={<Text bold align="center">Weather</Text>}>
         <Toggle
            settingsKey="unitToggle"
-           label="Tempterature in °F or °C"
-         />
+           label="US or Metric Units" 
+           onChange={value => props.settingsStorage.setItem('unit', value.toString())}
+        />
+        <Text>Temperatures in degrees {props.settingsStorage.getItem('unit') == "true" ? "celsius" : "fahrenheit"}</Text>
+      
         <Toggle
            settingsKey="weatherScrollToggle"
            label="Disable weather scrolling"
-         />
-         <Toggle
+        />
+        <Toggle
            settingsKey="locationScrollToggle"
            label="Disable location scrolling"
-         />
+        />
         <Select
           label={`Weather Update Interval`}
           settingsKey="updateInterval"
@@ -61,9 +64,6 @@ function mySettings(props) {
       </Section>
       <Section
         title={<Text bold align="center">Separator Bar Color</Text>}>
-        <Text align="center">
-          Color when not over-ridden
-        </Text>
         <ColorSelect
           settingsKey="color"
           colors={[
@@ -120,7 +120,7 @@ function mySettings(props) {
       <Section
         title={<Text bold align="center">Contact Me</Text>}>
         <Text>
-          Please don't hesitiate to contact me with questions or suggestions; but be sure to let me know which app or watchface you are talking about. This and all my other apps will always be free and Open Source. If you really like my app please considder buying me a coffee (or more likely electonic components that end up in my classroom). Thanks!
+          Please don't hesitate to contact me with questions or suggestions; but be sure to let me know which app or watchface you are talking about. This and all my other apps will always be free and Open Source. If you really like my app please consider buying me a coffee (or more likely electronic components that end up in my classroom). Thanks!
         </Text>
         <Link source="https://rawgit.com/cmspooner/Kearsarge-Time-for-Fitbit-Ionic/master/settings/email.html">
           <TextImageRow
@@ -146,6 +146,9 @@ function mySettings(props) {
       </Section>
       <Section
         title={<Text bold align="center">Build Version and Notes</Text>}>
+        <Text>
+          1.4: Settings now shows units
+         </Text>
         <Text>
           1.3: Fixed disabled typo.
          </Text>
